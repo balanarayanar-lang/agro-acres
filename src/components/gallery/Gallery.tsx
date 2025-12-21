a4def2681754ee4406d6de4e239e4f1d767cbf74
moushmigrace <moushmigrace@gmail.com>
2025-12-22 00:18:20 +0530

    contactus from gallery

--- src/components/gallery/Gallery.tsx
+++ src/components/gallery/Gallery.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+import { Modal } from "antd";
 import Header from "../header/Header";
-import {GalleryImagesSection} from "./GalleryImagesSection";
-import {GalleryIntro} from "./GalleryIntro";
+import { GalleryImagesSection } from "./GalleryImagesSection";
+import { GalleryIntro } from "./GalleryIntro";
+import { ContactUs } from "../contact-us/ContactUs";
 
-const Gallery = () => (
-  <>
-    <Header />
-    <GalleryIntro />
-    <GalleryImagesSection />
-  </>
-);
+const Gallery = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <>
+      <Header onContactClick={() => setIsModalOpen(true)} />
+
+      <GalleryIntro />
+      <GalleryImagesSection />
+
+      <Modal
+        open={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        footer={null}
+        width={1400}
+        closable
+        style={{ top: 60 }}
+        styles={{
+          body: {
+            padding: "3rem 4rem",
+            backgroundColor: "#fdfcfb",
+          },
+        }}
+      >
+        <ContactUs />
+      </Modal>
+    </>
+  );
+};
 
 export default Gallery;
